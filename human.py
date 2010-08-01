8ebf6d22478ed46b46de3d6c8e3ff6c362fc9c14
Christian Oudard <christian.oudard@gmail.com>
2010-08-01 15:03:38 -0400

    Moved human interface to its own file.

---
+++ human.py
@@ -0,0 +1,99 @@
+import sys
+from copy import deepcopy
+from getch import getch, getch_arrow
+from pieces import piece_to_name, name_to_piece
+
+def display_board(board):
+    print('+' + '='*board.size + '+')
+    for line in str(board).split('\n'):
+        print('|' + line + '|')
+    print('+' + '='*board.size + '+')
+
+def get_input(prompt):
+    if not prompt[-1].isspace():
+        prompt += ' '
+    while True:
+        try:
+            command = input(prompt)
+            if command == 'exit':
+                sys.exit()
+            return command
+        except (KeyboardInterrupt, EOFError):
+            print()
+            confirmation = input('Are you sure you want to exit [Y / N]? ')
+            if confirmation.lower().startswith('y'):
+                sys.exit()
+
+def move(board, player, player_pieces):
+    print("{}'s turn.".format(player))
+
+    display_board(board)
+
+    # Choose piece.
+    my_pieces = player_pieces[player]
+    while True:
+        name = get_input('piece? >')
+        piece = name_to_piece.get(name)
+        if not piece:
+            print('Piece name not recognized.')
+        elif piece not in my_pieces:
+            piece = None
+            print('You have already played this piece.')
+        else:
+            break
+        print('Available pieces:',
+              ', '.join(piece_to_name[p] for p in player_pieces[player]))
+
+    # Choose orientation.
+    orientations = sorted(piece.orientations())
+    piece = orientations[0]
+    o_index = 0
+
+    # Place piece
+    location = board.size // 2, board.size // 2
+    while True:
+        temp_board = deepcopy(board)
+        try:
+            temp_board._check_place_piece(piece, location, player)
+        except ValueError as e:
+            print(e)
+            color = '#'
+        else:
+            color = '@'
+        temp_board._place_piece(piece, location, color)
+        display_board(temp_board)
+
+        print('Use arrow keys to move piece. Press space bar to change orientation. Press Enter to confirm, or escape to go back (STUB).')
+
+        command = getch_arrow()
+
+        if command == '\x03': # Ctrl-C
+            raise KeyboardInterrupt()
+
+        # Change location
+        x, y = location
+        if command == 'up':
+            y -= 1
+        if command == 'down':
+            y += 1
+        if command == 'left':
+            x -= 1
+        if command == 'right':
+            x += 1
+        x %= board.size
+        y %= board.size
+        location = x, y
+
+        # Change orientation.
+        if command == ' ': # Space key
+            o_index = (o_index + 1) % len(orientations)
+            piece = orientations[o_index]
+
+        # Go back.
+        if command == '\x1b': # Escape key
+            pass #STUB, go back to piece selection.
+
+        # Confirm.
+        if command == '\r': # Enter key
+            return piece, location
+
--- play.py
+++ play.py
@@ -1,35 +1,18 @@
 #! /usr/bin/env python3
 
-import sys
 import itertools
 from copy import deepcopy
 from pieces import all_pieces, piece_to_name, name_to_piece
 from board import Board
-from getch import getch, getch_arrow
-
-def display_board(board):
-    print('+' + '='*board.size + '+')
-    for line in str(board).split('\n'):
-        print('|' + line + '|')
-    print('+' + '='*board.size + '+')
-
-def get_input(prompt):
-    if not prompt[-1].isspace():
-        prompt += ' '
-    while True:
-        try:
-            command = input(prompt)
-            if command == 'exit':
-                sys.exit()
-            return command
-        except (KeyboardInterrupt, EOFError):
-            print()
-            confirmation = input('Are you sure you want to exit [Y / N]? ')
-            if confirmation.lower().startswith('y'):
-                sys.exit()
 
 if __name__ == '__main__':
+    import human
+
     players = ['X', 'O']
+    player_interfaces = {
+        'X': human,
+        'O': human,
+    }
     player_pieces = {
         'X': deepcopy(all_pieces),
         'O': deepcopy(all_pieces),
@@ -38,74 +21,11 @@ if __name__ == '__main__':
     board = Board()
 
     for player in itertools.cycle(players):
-        display_board(board)
-        print("{}'s turn.".format(player))
-
-        # Choose piece.
-        while True:
-            name = get_input('piece? >')
-            piece = name_to_piece.get(name)
-            if piece:
-                break
-            print('Piece name not recognized. Available pieces:',
-                  ', '.join(piece_to_name[p] for p in player_pieces[player]))
-
-        # Choose orientation.
-        orientations = sorted(piece.orientations())
-        piece = orientations[0]
-        o_index = 0
-
-        # Place piece
-        location = board.size // 2, board.size // 2
-        while True:
-            temp_board = deepcopy(board)
-            try:
-                temp_board._check_place_piece(piece, location, player)
-            except ValueError as e:
-                print(e)
-                color = '#'
-            else:
-                color = '@'
-            temp_board._place_piece(piece, location, color)
-            display_board(temp_board)
-
-            print('Use arrow keys to move piece. Press space bar to change orientation. Press Enter to confirm, or escape to go back (STUB).')
-
-            command = getch_arrow()
-
-            if command == '\x03': # Ctrl-C
-                raise KeyboardInterrupt()
-
-            # Change location
-            x, y = location
-            if command == 'up':
-                y -= 1
-            if command == 'down':
-                y += 1
-            if command == 'left':
-                x -= 1
-            if command == 'right':
-                x += 1
-            x %= board.size
-            y %= board.size
-            location = x, y
-
-            # Change orientation.
-            if command == ' ': # Space key
-                o_index = (o_index + 1) % len(orientations)
-                piece = orientations[o_index]
-
-            # Confirm.
-            if command == '\r': # Enter key
-                try:
-                    board.place_piece(piece, location, player)
-                except ValueError as e:
-                    print('Cannot place piece here.')
-                else:
-                    break
-
-            # Go back.
-            if command == '\x1b': # Escape key
-                pass #STUB, go back to piece selection.
-
-
+        piece, location = player_interfaces[player].move(board, player, player_pieces)
+        available_pieces = player_pieces[player]
+        c_piece = piece.canonical()
+        if c_piece in available_pieces:
+            available_pieces.remove(c_piece)
+        else:
+            raise ValueError('Piece not available.')
+        board.place_piece(piece, location, player)
